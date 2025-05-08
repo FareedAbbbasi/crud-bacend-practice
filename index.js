2a03e33fe610c48800689146edc08ff69fe3cc72
FareedAbbbasi <codingabbas0999@gmail.com>
2025-05-08 20:57:15 +0500

    bux fix and implement the api in the backend

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ app.post("/student-insert", async (req, res) => {
 app.delete("/student-delete/:id", async(req, res) => {
     let {id} = req.params
     let myDB = await dbConnection();
-    let sudentDelete = myDB.collection("students");
-    let deleteid = await sudentDelete.deleteOne({_id: new ObjectId(id)})
+    let studentCollection = myDB.collection("students");
+    let deleteid = await studentCollection.deleteOne({_id: new ObjectId(id)})
     res.send({
         status: 1,
         msg: "Data delete successfuly!",
@@ -49,10 +49,26 @@ app.delete("/student-delete/:id", async(req, res) => {
 
 app.put("/studennt-update/:id", async(req, res) => {
     let {id} = req.params
+    let {sName, sEmail} = req.body
+    let obj={}
+    if(sName!=="" && sName!==undefined && sName!==null){
+        obj["sName"] = sName
+    }
+    if(sEmail!=="" && sEmail!==undefined && sEmail!==null) {
+        obj["sEmail"] = sEmail
+    }
+
+    console.log(obj)
     let myDB = await dbConnection();
     let studentData = myDB.collection("students");
-    let updateData = await studentData.updateOne({_id: new ObjectId(id)})
+    let updateData = await studentData.updateOne({_id: new ObjectId(id)}, {$set:obj})
 
+    resObj = {
+        status: 1, 
+        msg: "Data Update", 
+        updateData
+    }
+    res.send(resObj)
 })
 
 app.listen(process.env.PORT || "8000")
